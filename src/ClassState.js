9a792999c5a551a551dae8781f3ad4cf0e52cc40
Carlos Alfredo Castillo Rodríguez <cacr1990@gmail.com>
2022-01-19 17:39:42 -0300

    Estados compuestos con React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            value: '',
             error: false,
             loading: false,
         }
@@ -26,7 +29,11 @@ class ClassState extends React.Component {
             setTimeout(() => {
                 console.log('Haciendo la validación');
 
-                this.setState({ loading: false })
+                if (SECURITY_CODE === this.state.value) {
+                    this.setState({ error: false, loading: false })
+                } else {
+                    this.setState({ error: true, loading: false })
+                }
 
                 console.log('Terminando la validación')
             }, 3000)
@@ -34,17 +41,22 @@ class ClassState extends React.Component {
     }
 
     render() {
+        const { error, loading, value } = this.state;
         return (
             <div>
                 <h2>Eliminar { this.props.name }</h2>
                 <p>Por favor, escribe el código de seguridad.</p>
-                {this.state.error && (
+                {(error && !loading) && (
                     <p>Error: el código es incorrecto</p>
                 )}
-                {this.state.loading && (
+                {loading && (
                     <Loading />
                 )}
-                <input placeholder='Código de seguridad'/>
+                <input onChange={(event) => {
+                    this.setState({ value: event.target.value });
+                }}
+                    placeholder='Código de seguridad'
+                    value = { value }/>
                 <button onClick={
                     () => this.setState( () => this.setState({ loading: true }) )
                 }>Comprobar</button>
