5c75e97d6f70f8ba5f70ef892dca8b732fa1cd62
Carlos Alfredo Castillo Rodríguez <cacr1990@gmail.com>
2022-06-07 19:13:54 -0400

    ADD: imperative states

--- src/UseState.js
+++ src/UseState.js
@@ -6,7 +6,9 @@ function UseState ({ name }) {
     const [state, setState] = React.useState({
         value: '',
         error: false,
-        loading: false
+        loading: false,
+        deleted: false,
+        confirmed: false
     });
 
     console.log(state.value);
@@ -15,11 +17,12 @@ function UseState ({ name }) {
         if (!!state.loading) {
             setTimeout(() => {
                 console.log('Haciendo la validación');
-                 if (state.value !== SECURITY_CODE){
+                 if (state.value === SECURITY_CODE){
                      setState({ 
-                         ...state,
-                         error: false,
-                        loading: false
+                        ...state,
+                        error: false,
+                        loading: false,
+                        confirmed: true
                      });
                  } else {
                     setState({ 
@@ -32,35 +35,68 @@ function UseState ({ name }) {
         }
 
         console.log('Terminando el efecto');
-    },[ state ])
+    },[ state.loading ])
 
-    return  (
-        <div>
-            <h2>Eliminar { name }</h2>
-            <p>Por favor, escribe el código de seguridad.</p>
-            {(state.error && !state.loading) && (
-                <p>Error: el código es incorrecto</p>
-            )}
-            {state.loading && (
-                <p>Cargando</p>
-            )}
-            <input
-                onChange={(event) => {
+    console.log(JSON.stringify(state));
+
+    if (!state.deleted && !state.confirmed) {
+        return  (
+            <div>
+                <h2>Eliminar { name }</h2>
+                <p>Por favor, escribe el código de seguridad.</p>
+                {(state.error && !state.loading) && (
+                    <p>Error: el código es incorrecto</p>
+                )}
+                {state.loading && (
+                    <p>Cargando</p>
+                )}
+                <input
+                    onChange={(event) => {
+                        setState({
+                            ...state, 
+                            value: event.target.value 
+                        })
+                    }}
+                    placeholder = 'Código de seguridad'
+                    value = { state.value }/>
+                <button onClick={() => {
                     setState({
-                        ...state, 
-                        value: event.target.value 
+                        ...state,
+                        loading: true
                     })
-                }}
-                placeholder = 'Código de seguridad'
-                value = { state.value }/>
-            <button onClick={() => {
-                setState({
-                    ...state,
-                    loading: true
-                })
-            }} >Comprobar</button>
-        </div>
-    )
+                }} >Comprobar</button>
+            </div>
+        )
+    } else if (!!state.confirmed && !state.deleted) {
+        return (
+            <React.Fragment>
+                <p>Pedimos confirmación. ¿Estas seguro?</p>
+                <button
+                    onClick={() => {
+                        setState({
+                            ...state,
+                            deleted: true
+                        })
+                    }}
+                >Sí, eliminar</button>
+                <button
+                    onClick={() => {
+                        setState({
+                            ...state,
+                            confirmed: true
+                        })
+                    }}
+                >No, me arrepentí</button>
+            </React.Fragment>
+        );
+        
+    } else {
+        return (
+            <React.Fragment>
+                <p>Eliminado con exito</p>
+            </React.Fragment>
+        );
+    }
 }
 
 export { UseState }
